c2108f2524971372b280060d36ee6ee8cb190637
Karl Lundstig <karl.lundstig@gmail.com>
2017-04-22 15:08:03 +0200

    Better movement

--- src/util.cpp
+++ src/util.cpp
@@ -14,8 +14,7 @@ void loadTexture(sf::Texture& texture, string file) {
 }
 
 void drawSprite(sf::Sprite& sprite, Game* game) {
-    sf::FloatRect rect = sprite.getLocalBounds();
-    sprite.setPosition(sprite.getPosition() + game->center + game->screenCenter - sf::Vector2f(rect.width / 2.0f, rect.height / 2.0f));
+    sprite.setPosition(sprite.getPosition() + game->center + game->screenCenter);
     game->window->draw(sprite);
 }
 
